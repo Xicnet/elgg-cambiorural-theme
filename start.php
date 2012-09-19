dfd8eee1caae26f818cdcba0fd18213c0e82be0f
XICnet <code@xicnet.com>
2012-09-19 12:06:36 -0300

    Add custom favicon

--- start.php
+++ start.php
@@ -6,14 +6,38 @@
 elgg_register_event_handler('init', 'system', 'cambiorural_theme_init');
 
 function cambiorural_theme_init() {
-	// Extend CSS
-	elgg_extend_view('css/elgg', 'cambiorural_theme/css');
+    // Extend CSS
+    elgg_extend_view('css/elgg', 'cambiorural_theme/css');
 
     // Remove Elgg logo
-	elgg_unregister_menu_item('topbar', 'elgg_logo');
+    elgg_unregister_menu_item('topbar', 'elgg_logo');
 
-	// See views for more
+    // Page Handler
+    elgg_register_page_handler('cambiorural_theme', 'cambiorural_theme_page_handler');
+
+    // See views for more
 }
 
 function cambiorural_theme_test() {}
 
+function cambiorural_theme_page_handler($page) {
+
+	switch ($page[0]) {
+	case 'favicon':
+		cambiorural_theme_handle_favicon();
+		break;
+	default:
+		return FALSE;
+	}
+
+}
+
+function cambiorural_theme_handle_favicon() {
+	$favicon = elgg_get_plugins_path() . 'cambiorural_theme/graphics/favicon.ico';
+	if (is_readable($favicon)) {
+		header('Content-Type: image/x-icon');
+		header('Content-Length: ' . filesize($favicon));
+		readfile($favicon);
+		exit;
+	}
+}
